b029257ad50ff4b262149825208f827fe58fdde2
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-02-17 11:17:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";	
+}
